fdf11861f7514a60e4d7c20caa04864a47f68778
Mark Durrant <mark.j.durrant@gmail.com>
2023-07-13 10:46:09 +0100

    AG-3390 - Homepage darkmode improvements

--- grid-packages/ag-grid-docs/documentation/src/pages/index.jsx
+++ grid-packages/ag-grid-docs/documentation/src/pages/index.jsx
@@ -174,7 +174,7 @@ const Default = () => {
                             </a>
                         </li>
 
-                        <li className={styles.project}>
+                        <li className={classNames(styles.project, styles.projectPlunker)}>
                             <img src="images/fw-logos/plunker.svg" alt="Plunker" />
 
                             <h3>Plunker</h3>
